874501ac99bc862f5bcaed638beb6a98ce925eb7
John Zimmerman <john@meetjohnz.com>
2025-09-16 21:49:49 -0500

    code exercise chapter 4 review

--- code_examples/main.cpp
+++ code_examples/main.cpp
@@ -1,15 +1,43 @@
 #include <iostream>
 
-void printSize(int x) {
-  std::cout << sizeof(x) << '\n';
+double getTowerHeight() {
+  std::cout << "Enter the height of the tower in meters: ";
+  double height{};
+  std::cin >> height;
+  return height;
+}
+
+double getBallHeight(double towerHeight, float seconds) {
+  double groundDistance{towerHeight - (4.9 * (seconds * seconds))};
+
+  if (groundDistance < 0.0)
+    return 0.0;
+  return groundDistance;
+}
+
+void printHeight(double groundDistance, float seconds) {
+  if (groundDistance > 0) {
+    std::cout << "At " << seconds << " seconds, the ball is at height: " << groundDistance << " meters.\n";
+  } else {
+    std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
+  }
+}
+
+void getAndPrintGroundDistance(double towerHeight, float seconds) {
+  double groundDistance{getBallHeight(towerHeight, seconds)};
+  printHeight(groundDistance, seconds);
 }
 
 int main() {
 
-  int num{32};
-  printSize(num);
+  double towerHeight{getTowerHeight()};
 
-  std::cout << sizeof(char) << '\n';
+  getAndPrintGroundDistance(towerHeight, 0.0f);
+  getAndPrintGroundDistance(towerHeight, 1.0f);
+  getAndPrintGroundDistance(towerHeight, 2.0f);
+  getAndPrintGroundDistance(towerHeight, 3.0f);
+  getAndPrintGroundDistance(towerHeight, 4.0f);
+  getAndPrintGroundDistance(towerHeight, 5.0f);
 
   return 0;
 }
--- todo/todo.md
+++ todo/todo.md
@@ -1,7 +1,8 @@
 ### Todo Sep 11 - Sep 17
 
-1. Finish chapter 4
-2. Finish Chapter 4 Quiz
-3. Start Chapter 5
-4. update /zettel-note with notes from /odds-ends
-
+- [x] Finish chapter 4
+- [ ] Finish Chapter 4 Quiz
+- [ ] Start Chapter 5
+- [ ] update /zettel-note with notes from /odds-ends
+- [ ] Add short definition of fixed-width integers into zettel note.
+ 
