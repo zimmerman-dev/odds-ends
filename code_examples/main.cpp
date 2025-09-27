e04706dd0851a794eea49071e2bd7e26329487ca
John Zimmerman <140193884+zimmerman-dev@users.noreply.github.com>
2025-09-26 23:33:58 -0500

    code-examples: updated

--- code_examples/main.cpp
+++ code_examples/main.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
-#include <string>
-#include <string_view>
-
-void printString(std::string test) {
-  std::cout << test << '\n';
-}
+#include <cmath>
 
 int main() {
+  int x{2};
+  int y{5};
 
-  std::string_view arg1{"Testing C-Style Literal"};
-  std::string arg2{arg1};
-
-  std::cout << typeid(arg1).name() << '\n';
-  std::cout << typeid(arg2).name() << '\n';
-  std::cout << typeid(static_cast<std::string>(arg1)).name();
-
-  printString(arg2);
-  printString(static_cast<std::string>(arg1));
+  int result{std::pow(x,y)};
+  std::cout << result;
   return 0;
 }
